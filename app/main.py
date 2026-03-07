bec1431f9eae9d859014409fb7013fb888a13cdb
Pulya063 <leatherstar07@gmail.com>
2026-03-07 22:09:17 +0100

    Version v3

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from app.db.database import engine, Base
 from app.router.router import meal
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 app = Flask(__name__)
-app.secret_key = "secret_key"
+app.secret_key = os.getenv("APP_SECRET_KEY")
 
 Base.metadata.create_all(bind=engine)
 app.register_blueprint(meal, url_prefix='/')
